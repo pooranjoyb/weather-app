49d3bc39bc2d1e6bd2d6c0b4d8f7d32cfcacfd77
pooranjoyb <pooranjoyb2016@gmail.com>
2022-12-20 23:58:47 +0530

    Added Error search Checks

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '098adbcc37msh7245f958e5aebfap1d25fcjsn32eabc9b69da',
-		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-	}
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '098adbcc37msh7245f958e5aebfap1d25fcjsn32eabc9b69da',
+        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
 };
 
 
-const getWeather = (city) =>{
+const getWeather = (city) => {
+    if (city === "") {
+        window.alert("Enter Valid City Name")
+    } else {
+        fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+        .then(response => response.json())
+        .then(response => {
+            
+            if (response.temp === undefined) {
+                window.alert("Enter Valid City Name")
+            }
+            else {
+                    cityName.innerHTML = city
+                    cloud_pct.innerHTML = response.cloud_pct
+                    temp.innerHTML = response.temp
+                    feels_like.innerHTML = response.feels_like
+                    humidity.innerHTML = response.humidity
+                    wind_speed.innerHTML = response.wind_speed
+                    wind_degrees.innerHTML = response.wind_degrees
+                    sunrise.innerHTML = new Date(response.sunrise * 1000)
+                    sunset.innerHTML = new Date(response.sunset * 1000)
 
-    cityName.innerHTML = city
-
-    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
-	.then(response => response.json())
-	.then(response => {        
-        cloud_pct.innerHTML = response.cloud_pct
-        temp.innerHTML = response.temp
-        feels_like.innerHTML = response.feels_like
-        humidity.innerHTML = response.humidity
-        wind_speed.innerHTML = response.wind_speed
-        wind_degrees.innerHTML = response.wind_degrees
-        sunrise.innerHTML = new Date(response.sunrise * 1000)
-        sunset.innerHTML = new Date(response.sunset * 1000)
-        
-    })
-	.catch(err => console.error(err));
+                }
+            })
+            .catch(err => console.error(err));
+    }
 }
 
-submit.addEventListener("click",  (e) => {
+submit.addEventListener("click", (e) => {
     e.preventDefault();
     getWeather(city.value);
 })
